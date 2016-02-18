e91c786e221ebe0e47a7ff54a226600ac83d1e96
YI JIANG <hawaiidoll68@gmail.com>
2016-02-17 23:23:20 -0800

    Update routes.rb

--- firstapp/config/routes.rb
+++ firstapp/config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+   get 'static_page/about'
+
+   get 'static_page/contact'
+
+   get 'static_page/index'
+
+   root  'static_page#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   # Serve websocket cable requests in-process
