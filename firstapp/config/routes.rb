47755cb69bd8c106a8a4b761ec5e88481c1e2c45
jennytech <hawaiidoll68@gmail.com>
2016-02-18 21:38:41 -0800

    add descript of change

--- firstapp/config/routes.rb
+++ firstapp/config/routes.rb
@@ -1,29 +1,14 @@
 Rails.application.routes.draw do
-<<<<<<< HEAD
-  
-  get 'static_pages/index'
+   get 'static_pages/about'
 
-  get 'static_pages/about'
+   get 'static_pages/contact'
 
-  get 'static_pages/contact'
+   get 'static_pages/index'
 
-  
-
- root  'static_pages#index'
-
-=======
-   get 'static_page/about'
-
-   get 'static_page/contact'
-
-   get 'static_page/index'
-
-   root  'static_page#index'
->>>>>>> 6b024b9da45ec7bfc567eff61406ccce123e4ca6
+   root  'static_pages#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   # Serve websocket cable requests in-process
   # mount ActionCable.server => '/cable'
 end
-
 
