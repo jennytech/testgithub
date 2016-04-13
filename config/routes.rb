4247db5073b5439e422126551a30d0aa7dd2e1db
jennytech <hawaiidoll68@gmail.com>
2016-04-13 10:01:29 -0700

    delete a root

--- config/routes.rb
+++ config/routes.rb
@@ -11,9 +11,13 @@ Rails.application.routes.draw do
 
    resources :orders, only: [:index, :show, :create, :destroy]
 
-   root  'static_pages#index'
+ 
 
    root 'static_pages#landing_page'
+
+
+
+
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
