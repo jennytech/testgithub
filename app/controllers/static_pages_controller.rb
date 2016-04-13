1e23fe6a8c7d2e2e2ee47129e025b0a2301beb55
jennytech <hawaiidoll68@gmail.com>
2016-04-12 22:30:22 -0700

    redirect and make dynamic link to the page with change routes

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,13 @@
 class StaticPagesController < ApplicationController
+	def index
+  end
+
+  def about
+  end
+
+  def contact
+  end
+  
   def landing_page
   	@products = Products.limit(3)
   end
