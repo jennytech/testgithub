18e4d03cd72b95bcf24e2a756cdb069d34dcda8c
jennytech <hawaiidoll68@gmail.com>
2016-05-03 22:43:32 -0700

    add review to the page

---
+++ app/assets/javascripts/site.js
@@ -0,0 +1,13 @@
+$(document).on("ready page:load", function(){
+	
+	$('.rating').raty( { path: '../assets/raty', scoreName: 'comment[rating]' });
+	$(".rated").raty( { path: '../assets/raty',
+      readOnly: true,
+      score: function() {
+      	return $(this).attr('data-score');
+      }
+
+
+	 });
+});
+
