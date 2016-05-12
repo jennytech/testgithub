36055c7fbc48a42c878228076dff3e796f28e446
jennytech <hawaiidoll68@gmail.com>
2016-05-12 11:11:57 -0700

    delete first app and edit product_spec.rb

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,26 +1,26 @@
 require 'rails_helper'
 
-describe Product do
+describe Product  do 
   context "when the product has comments" do
     before do
       @product = Product.create!(name: "race bike")
-      @user = FactoryGirl.create(:user)
-      @product.comments.create!(rating: 1, user: @user, body: "Awful bike!")
+      @user = User.create!(first_name: "BDD", last_name: "Test",email: "jenny-tech@outlook.com", password: "23s8977")
+      @product.comments.create!(rating: 1, user: @user, body: "Awful bike")
       @product.comments.create!(rating: 3, user: @user, body: "Good bike!")
       @product.comments.create!(rating: 5, user: @user, body: "Amazing bike!")
     end
-
+  
     it "returns the average rating of all comments" do
-      expect(@product.average_rating).to eq 3.0
+      expect(@product.average_rating).to eq 3
     end
   end
 
-  context "when a new product has no name" do
+  context "is not valid" do
     before do
       Product.new(description: "Nice bike")
     end
 
-    it "is not valid" do
+    it "should return not valid " do
       expect(Product.new(description: "Nice bike")).not_to be_valid
     end
   end
